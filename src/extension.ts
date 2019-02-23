0f0181558e444203c737aa97c8a0ff286c12a575
allyusd <allyusd@yahoo.com.tw>
2019-02-23 17:10:16 +0800

    feat: replace clipboard text include underscore and space, keybinding ctrl+shift+f12

--- src/extension.ts
+++ src/extension.ts
@@ -13,11 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('rfhelp.replaceClipboardTextIncludeUnderscoreAndSpace', () => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		replaceClipboardTextIncludeUnderscoreAndSpace();
 	});
 
 	context.subscriptions.push(disposable);
@@ -25,3 +24,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function replaceClipboardTextIncludeUnderscoreAndSpace() {
+	vscode.env.clipboard.readText().then((text)=>{
+		console.log('before: ' + text);
+		if (text.includes(' '))	{
+			text = text.replace(/ /g, '_');
+		} else if (text.includes('_')) {
+			text = text.replace(/_/g, ' ');
+		}
+		console.log('after: ' + text);
+		vscode.env.clipboard.writeText(text);
+	});
+}
