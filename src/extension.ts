b61e947e168ed710c699430dc2801b512cf8e086
allyusd <allyusd@yahoo.com.tw>
2019-02-25 15:25:24 +0800

    feat: Change Select Text To Robot Framework Case

--- src/extension.ts
+++ src/extension.ts
@@ -14,8 +14,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('rfhelper.replaceClipboardTextIncludeUnderscoreAndSpace', replaceClipboardTextIncludeUnderscoreAndSpace);
+	let disposable2 = vscode.commands.registerCommand('rfhelper.changeSelectTextToRFCase', changeSelectTextToRFCase);
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
@@ -25,6 +27,22 @@ function replaceClipboardTextIncludeUnderscoreAndSpace() {
 	vscode.env.clipboard.readText().then((text)=>{
 		text = replaceUnderscoreAndSpace(text);
 		vscode.env.clipboard.writeText(text);
+	});
+}
+
+function changeSelectTextToRFCase() {
+	var editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		console.log('No open text editor');
+		return;
+	}
+
+	var selection = editor.selection;
+	var text = editor.document.getText(selection);
+	text = changeTextToRFCase(text);
+	editor.edit(function(editBuilder)
+	{
+		editBuilder.replace(selection, text);
 	});
 }
 
@@ -39,4 +57,25 @@ function replaceUnderscoreAndSpace(text: string) {
 	let after: string = text;
 	console.log('change: ' + before + ' => ' + after);
 	return text;
+}
+
+function changeTextToRFCase(text: string) {
+	let before: string = text;
+	let list: string[];
+
+	text = text.replace(/_/g, ' ');
+
+	list = text.split(' ');
+	list.forEach(function(item, index) {
+		list[index] = capitalizeFirstLetter(item);
+	});
+	text = list.join(' ');
+
+	let after: string = text;
+	console.log('change: ' + before + ' => ' + after);
+	return text;
+}
+
+function capitalizeFirstLetter(text: string) {
+	return text.charAt(0).toUpperCase() + text.slice(1);
 }
